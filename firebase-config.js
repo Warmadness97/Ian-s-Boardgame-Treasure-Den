07a10c94d4cd5f8bf4e60728280e6c8fe53255d5
Warmadness97 <ianlin1997@gmail.com>
2026-07-19 16:16:22 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,15 @@
 // 詳細步驟請看 README.md
 
 export const firebaseConfig = {
-  apiKey: "請貼上你的 apiKey",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:xxxxxxxxxxxxxxxxxxxxxx"
+    apiKey: "AIzaSyDuIaf7MLGGm57VN7ueNiVyffnqpyDzqt8",
+    authDomain: "ian-s-boardgame-treasure-den.firebaseapp.com",
+    databaseURL: "https://ian-s-boardgame-treasure-den-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ian-s-boardgame-treasure-den",
+    storageBucket: "ian-s-boardgame-treasure-den.firebasestorage.app",
+    messagingSenderId: "455233771867",
+    appId: "1:455233771867:web:40971c122ac0112128627e"
 };
+
 
 // 只有這個信箱登入後才能新增／編輯／刪除桌遊與修改標題，
 // 其他人登入後仍只能瀏覽與搜尋。
